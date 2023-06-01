dd5edcbcccef9e20322f97475f994a93221ff14b
github-olivier-abdesselam <olivier.abdesselam@teads.com>
2023-06-01 07:15:36 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,273 @@
+use log::info;
+use web_sys::{HtmlInputElement, InputEvent};
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct Query {
+    content: String,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize, Clone)]
+struct Musical {
+    id: u64,
+    name: String,
+    url: String,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize, Clone)]
+struct ListItem {
+    id: u64,
+    musical_id: u64,
+    viewed: bool,
+    rating: u8,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize, Clone)]
+struct MusicaList {
+    version: u8,
+    author: String,
+    items: Vec<ListItem>,
+}
+
+#[derive(Debug, Clone, PartialEq, Routable)]
+pub enum Route {
+    #[at("/")]
+    Home,
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! {
+            <Home />
+        },
+    }
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    }
+}
+
+#[function_component(Home)]
+fn home() -> Html {
+    wasm_logger::init(wasm_logger::Config::default());
+    let default_list = MusicaList {
+        version: 1,
+        author: "".to_string(),
+        items: vec![],
+    };
+
+    let musicals_csv = include_str!("musicals.csv");
+
+    let bookmark_url = use_state(|| "".to_string());
+
+    let current_location = use_location().unwrap();
+
+    let content = current_location
+        .query::<Query>()
+        .map_or("".to_string(), |query| query.content);
+    let list_value: MusicaList = base64::decode(content)
+        .map(|bytes| rmp_serde::from_read_ref(&bytes).unwrap_or(default_list.clone()))
+        .unwrap_or(default_list);
+
+    let list = use_state(|| list_value.clone());
+
+    let delete = |id| {
+        let list = list.clone();
+        move |_| {
+            list.set(MusicaList {
+                version: list.version,
+                author: list.author.clone(),
+                items: list
+                    .items
+                    .iter()
+                    .filter(|item| item.id != id)
+                    .cloned()
+                    .collect(),
+            });
+        }
+    };
+
+    fn update_item_in_list(
+        list: &MusicaList,
+        id: u64,
+        f: impl Fn(&ListItem) -> ListItem,
+    ) -> MusicaList {
+        MusicaList {
+            version: list.version,
+            author: list.author.clone(),
+            items: list
+                .items
+                .iter()
+                .map(
+                    |item: &ListItem| {
+                        if item.id == id {
+                            f(item)
+                        } else {
+                            item.clone()
+                        }
+                    },
+                )
+                .collect(),
+        }
+    }
+
+    let change_viewed = |id| {
+        let list = list.clone();
+        move |_| {
+            list.set(update_item_in_list(&list, id, |item| ListItem {
+                viewed: !item.viewed,
+                ..item.clone()
+            }));
+        }
+    };
+
+    let update_rating = |id: u64, delta: i8| {
+        let list = list.clone();
+        move |_| {
+            list.set(update_item_in_list(&list, id, |item| ListItem {
+                rating: {
+                    let new_rating = item.rating as i8 + delta;
+                    if new_rating < 0 {
+                        10
+                    } else if new_rating > 10 {
+                        0
+                    } else {
+                        new_rating as u8
+                    }
+                },
+                ..item.clone()
+            }));
+        }
+    };
+
+    let change_musical = |id: u64| {
+        let list = list.clone();
+        move |e: Event| {
+            list.set(update_item_in_list(&list, id, |item| ListItem {
+                musical_id: e
+                    .target_unchecked_into::<HtmlInputElement>()
+                    .value()
+                    .parse::<u64>()
+                    .unwrap(),
+                ..item.clone()
+            }))
+        }
+    };
+
+    let generate_bookmark_url = {
+        let url = bookmark_url.clone();
+        let list = list.clone();
+        move |_| {
+            let val = MusicaList {
+                version: list.version,
+                author: list.author.clone(),
+                items: list.items.clone(),
+            };
+            let str = rmp_serde::to_vec(&val).unwrap();
+            // convert str to base64
+            let str = base64::encode(str);
+            let path = format!("/?content={}", str);
+            url.set(path);
+        }
+    };
+
+    let update_author = {
+        let list = list.clone();
+        Callback::from(move |e: InputEvent| {
+            list.set(MusicaList {
+                version: (*list).clone().version,
+                author: e
+                    .target_unchecked_into::<HtmlInputElement>()
+                    .value()
+                    .clone(),
+                items: (*list).clone().items,
+            })
+        })
+    };
+
+    let add_musical = {
+        let list = list.clone();
+        move |_| {
+            let mut items = list.items.clone();
+            items.push(ListItem {
+                id: list.items.len() as u64 + 1,
+                musical_id: 1,
+                viewed: false,
+                rating: 0,
+            });
+            list.set(MusicaList {
+                version: list.version,
+                author: list.author.clone(),
+                items,
+            });
+        }
+    };
+
+    let mut musicals: Vec<Musical> = vec![];
+
+    info!("{}", musicals_csv);
+
+    let mut reader = csv::Reader::from_reader(musicals_csv.as_bytes());
+    for record in reader.deserialize::<Musical>() {
+        match record {
+            Ok(musical) => musicals.push(musical),
+            Err(err) => info!("{:?}", err),
+        };
+    }
+
+    html! {
+        <>
+            <h1>
+            <input type="text" value={ (*list).clone().author } oninput={update_author}/>
+
+            { "'s MusicaList"}</h1>
+        <table border=1>
+            <tr>
+                <th>{ "Musical" }</th>
+                <th>{ "Viewed in person" }</th>
+                <th>{ "Rating"}</th>
+                <th>{ "actions" }</th>
+            </tr>
+            { for (*list).clone().items.iter().map(|item| {
+                html! {
+                    <tr>
+                        <td>
+                        <select onchange={change_musical(item.id)}>
+                            { for musicals.iter().map(|m| {
+                                html! {
+                                    <option value={ format!("{}", m.id) } selected={ m.id == item.musical_id }>{ &m.name }</option>
+                                }
+                            })}
+                        </select>
+                                </td>
+                        <td><input type="checkbox" value={ format!("{}", item.viewed) } onchange={change_viewed(item.id)}/></td>
+                        <td>{ item.rating }</td>
+                        <td>
+                        <button onclick={update_rating(item.id, 1)}>{ "+" } </button>
+                        <button onclick={update_rating(item.id, -1)}>{ "-" } </button>
+                        <button onclick={delete(item.id)}>{ "🗑 " } </button>
+                        </td>
+                    </tr>
+                }
+            })}
+        </table>
+        <button onclick={add_musical}>{ "➕" } </button>
+            <hr/>
+        <div>
+            <p><a href={ (*bookmark_url).clone() }>{"bookmark link"}</a></p>
+            <button onclick={generate_bookmark_url}>{ "update bookmark link" }</button>
+        </div>
+        <hr/>
+        </>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
