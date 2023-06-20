bcf37950494587a26da377e6bef5723bc5398048
yazgoo <yazgoo@users.noreply.github.com>
2023-06-20 22:39:09 +0000

    deploy: cb0287bdc9ec5433d363cd25fc1890bd8c104f2a

--- musicalist-c15ab65f62ecb4b3.js
+++ musicalist-c15ab65f62ecb4b3.js
@@ -888,7 +888,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('musicalist-898d864746dcf313_bg.wasm', import.meta.url);
+        input = new URL('musicalist-c15ab65f62ecb4b3_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
